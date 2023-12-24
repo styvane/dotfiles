6ad1bed2e0dcb9e56907b6c81c0df590488683e0
Sede Soukossi <4968379+styvane@users.noreply.github.com>
2023-12-24 15:05:35 +0100

    Do not sync extra

--- bootstrap.sh
+++ bootstrap.sh
@@ -20,6 +20,7 @@ function doIt() {
 		--exclude "firefox.desktop" \
 		--exclude "install.sh" \
 		--exclude "jon-tyson-unsplash.jpg" \
+                --exclude ".extra" \
 	        -avh --no-perms . ~;
 	else
 	    rsync --exclude ".git/" \
@@ -32,6 +33,7 @@ function doIt() {
 		--exclude "firefox.desktop" \
 		--exclude "install.sh" \
 		--exclude "jon-tyson-unsplash.jpg" \
+                --exclude ".extra" \
 		-avh --no-perms . ~;
 
 	fi
