c0b09e1d3dfcfd5cafcff98a89b8f8f1fe75e4f9
Sede Soukossi <4968379+styvane@users.noreply.github.com>
2025-01-02 15:14:36 +0100

    Add ghc installation env

--- install.sh
+++ install.sh
@@ -13,6 +13,9 @@ fi
 ./install_rs.sh
 
 # Install GHc
+BOOTSTRAP_HASKELL_NONINTERACTIVE=1 \
+BOOTSTRAP_HASKELL_INSTALL_HLS=Y \
+BOOTSTRAP_HASKELL_ADJUST_BASHRC=N \
 curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | sh
 
 # Install uv
