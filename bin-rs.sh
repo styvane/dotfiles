10765a9d21ba0caf716427e1d11118e7f14476a1
Sede Soukossi <4968379+styvane@users.noreply.github.com>
2025-02-01 20:56:32 +0100

    Added rust bin script

---
+++ bin-rs.sh
@@ -0,0 +1,3 @@
+# eza
+alias ls='eza --classify=auto --color --group-directories-first --sort=extension -A'
+alias la='eza --classify=auto --color --group-directories-first --sort=extension -a --octal-permissions --no-permissions'
