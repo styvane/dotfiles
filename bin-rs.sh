b8ad75ea942eb47bbb924bfe45255a900e7b0750
Sede Soukossi <4968379+styvane@users.noreply.github.com>
2025-02-05 21:07:47 +0100

    Remove bin-rs.sh

--- bin-rs.sh
+++
@@ -1,3 +0,0 @@
-# eza
-alias ls='eza --classify=auto --color --group-directories-first --sort=extension -A'
-alias la='eza --classify=auto --color --group-directories-first --sort=extension -a --octal-permissions --no-permissions'
